df220e693d9579eaf9c1d7dfa6987b212b6e7465
indianshadowking <pruthvirajsv@gmail.com>
2020-05-26 20:07:20 +0530

    beta 3.0

--- Code/timetable_with_time.cpp
+++ Code/timetable_with_time.cpp
@@ -19,7 +19,7 @@ void pattern();
 
 int main(){
 	int total_subjects,n=0,min,pos,i,duration,time;
-	int J_hist[50];
+	int J_hist[150];
 	cout<<"Enter the total Number of Subjects\n";
 	cin>>total_subjects;
 	cout<<"Enter the starting time of Regular class\n";
@@ -31,9 +31,9 @@ int main(){
 	getdata(data,data_copy,total_subjects,name);
 	printdata(data,total_subjects,name);
 	int temp[total_subjects][10];
- 	int time_table[100][10][10];
+ 	int time_table[150][10][10];
 	dataselection(data_copy,total_subjects,temp,name,temp_name,teacher_name);
-	while(n<100 && repeat_same_hour_check(temp,data,total_subjects))
+	while(n<150 && repeat_same_hour_check(temp,data,total_subjects) && repeat_same_day_check(temp,data,total_subjects))
 	{
 		copy(n,temp,time_table,total_subjects);
 		J_hist[n]=cost_compute(temp,total_subjects,data);
@@ -43,11 +43,11 @@ int main(){
 	}
 	min=J_hist[0];
         pos=0;
-        for(i=0;i<100;i++){
-                if(J_hist[i]<=min){
-                        min=J_hist[i];
-                        pos=i;
-                }
+        for(i=0;i<n+1;i++){
+            if(J_hist[i]<=min){
+                min=J_hist[i];
+                pos=i;
+            }
         }
 
 	printtable(time_table[pos],total_subjects,data,temp_name,pos,time,duration,teacher_name);
@@ -80,7 +80,7 @@ void printdata(int data[][2],int total_subjects,string name[][2])
 	cout<<"\tSUBJECTS_CODE\tSUBJECT_CREDIT\tSUBJECT_NAME\t  SUBJECT_TEACHER\n";
         for(i=0;i<total_subjects;i++)
         {       
-                cout<<setw(14)<<data[i][0]<<setw(16)<<data[i][1]<<setw(19)<<name[i][0]<<setw(20)<<name[i][1]<<endl;
+            cout<<setw(14)<<data[i][0]<<setw(16)<<data[i][1]<<setw(19)<<name[i][0]<<setw(20)<<name[i][1]<<endl;
         }
 }
 
@@ -109,11 +109,11 @@ void dataselection(int data_copy[][2],int total_subjects,int temp[][10],string n
 
 int class_per_day(int data_copy[][2],int total_subjects)
 {
-        int i,m=0;
-        for(i=0;i<total_subjects;i++)
-            m+=data_copy[i][1];
-        m=(m/6)+1;
-        return m;
+    int i,m=0;
+    for(i=0;i<total_subjects;i++)
+        m+=data_copy[i][1];
+    m=(m/6)+1;
+    return m;
 }
 
 int getmax(int data_copy[][2],int total_subjects)
@@ -148,68 +148,68 @@ void printtable(int time_table[][10],int total_subjects,int data[][2],string tem
 	pattern();
 	myfile<<","<<"MONDAY"<<","<<"TUESDAY"<<","<<"WEDNESDAY"<<","<<"THURSDAY"<<","<<"FRIDAY"<<","<<"SATURDAY"<<endl;
     cout<<"\n\t\t  MONDAY   TUESDAY   WEDNESDAY   THURSDAY   FRIDAY   SATURDAY\n";
-        for(i=0;i<n;i++)
-        {
+    for(i=0;i<n;i++)
+    {
 	    if( cnt==0)
 	    {
 		    cout<<"["<<time<<" to "<<time+duration<<"]";
 		    myfile<<endl<< "["<<time<<" to "<<time+duration<<"]"<<endl;
-            	    time++;
+            time++;
 	    }
 	    if(cnt == 1)
 	    {
-		cout<<"["<<time<<":30"<<" to "<<time+1<<":30]";
+			cout<<"["<<time<<":30"<<" to "<<time+1<<":30]";
      		myfile<< "["<<time<<":30"<<" to "<<time+1<<":30]"<<endl;
-		time++;
+			time++;
 	    }
 	    cout<<"\nPeriod : "<<i+1;
-            myfile << "PERIOD : " << i+1 << ",";
-            for(j=0;j<6;j++)
-            {
-             	cout<<setw(11)<<temp_name[i][j];
-		myfile << temp_name[i][j] << ",";
-            }
-			cout<<endl;
-			myfile<<endl;
-			cout<<"Lecturer :";
-			myfile<<"Lecturer: "<<",";
-			for(j=0;j<6;j++)
-        		{
-        	        cout<<setw(11)<<teacher_name[i][j];
-        	        myfile<<teacher_name[i][j]<<",";
-				}
-			cout<<endl;
-			myfile<<endl;
-			if(i==1 && n>2)
-                {
-                    cout<<endl;
-                    pattern();
-                    cout<<"\n\t\t\t\tSHORT BREAK";
-                    myfile<<endl<<" ["<<time<<" to "<<time<<":30]"<<",";
-                    myfile<<"SHORT BREAK"<<","<<"SHORT BREAK"<<","<<"SHORT BREAK"<<","<<"SHORT BREAK";
-                    myfile<<","<<"SHORT BREAK"<<","<<"SHORT BREAK"<<endl;
-					cout<<" ["<<time<<" to "<<time<<":30]";
-			        cnt=1;
-					cout<<endl;
-                    pattern();
-                }
-                if(i==3 && n>4)
-                {
-                    cout<<endl;
-					pattern();
-					cout<<"\n\t\t\t\tLUNCH BREAK";
-					myfile<<endl<<" ["<<time<<":30"<<" to "<<time+1<<":30]";
-					myfile<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK";
-					myfile<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<endl;
-					cout<<" ["<<time<<":30"<<" to "<<time+1<<":30]";
-					time++;
-					cnt=1;
-					cout<<endl;
-					pattern();
-                }
-                myfile<<endl;
-                cout<<endl;
+        myfile << "PERIOD : " << i+1 << ",";
+        for(j=0;j<6;j++)
+        {
+            cout<<setw(11)<<temp_name[i][j];
+			myfile << temp_name[i][j] << ",";
         }
+		cout<<endl;
+		myfile<<endl;
+		cout<<"Lecturer :";
+		myfile<<"Lecturer: "<<",";
+		for(j=0;j<6;j++)
+        {
+            cout<<setw(11)<<teacher_name[i][j];
+            myfile<<teacher_name[i][j]<<",";
+		}
+		cout<<endl;
+		myfile<<endl;
+		if(i==1 && n>2)
+        {
+            cout<<endl;
+            pattern();
+            cout<<"\n\t\t\t\tSHORT BREAK";
+            myfile<<endl<<" ["<<time<<" to "<<time<<":30]"<<",";
+            myfile<<"SHORT BREAK"<<","<<"SHORT BREAK"<<","<<"SHORT BREAK"<<","<<"SHORT BREAK";
+            myfile<<","<<"SHORT BREAK"<<","<<"SHORT BREAK"<<endl;
+			cout<<" ["<<time<<" to "<<time<<":30]";
+		    cnt=1;
+			cout<<endl;
+            pattern();
+        }
+        if(i==3 && n>4)
+        {
+            cout<<endl;
+			pattern();
+			cout<<"\n\t\t\t\tLUNCH BREAK";
+			myfile<<endl<<" ["<<time<<":30"<<" to "<<time+1<<":30]";
+			myfile<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK";
+			myfile<<","<<"LUNCH BREAK"<<","<<"LUNCH BREAK"<<endl;
+			cout<<" ["<<time<<":30"<<" to "<<time+1<<":30]";
+			time++;
+			cnt=1;
+			cout<<endl;
+			pattern();
+        }
+        myfile<<endl;
+        cout<<endl;
+    }
 	pattern();
 	cout<<endl;
 }
